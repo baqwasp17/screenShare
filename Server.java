6503155b1c977de26e9bce86f9a576fee94c2e82
Arnav Tyagi <arnav.16bcs2017@abes.ac.in>
2019-04-13 13:34:30 +0530

    Initial Commit after adding basic Client and Server

---
+++ Server.java
@@ -0,0 +1,30 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.awt.Robot;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+
+public class Server{
+	public static void main(String args[])throws Exception{
+		int port = 7117;
+		boolean done = false;
+		Rectangle sharedScreen = 
+			new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+		Robot robot = new Robot();
+		ServerSocket serverSocket = new ServerSocket(port);
+		Socket socket = serverSocket.accept();
+		BufferedImage screenShot = robot.createScreenCapture(sharedScreen);
+		InputStream is = socket.getInputStream();
+		OutputStream os = socket.getOutputStream();
+		ImageIO.write(screenShot, "PNG", os);
+		socket.close();
+	}
+
+}
